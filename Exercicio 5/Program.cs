026098882d9fee693c81243674b435f7302bf32a
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:18:36 -0300

    Bool

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,5 @@
+﻿//Crie um programa que declare uma variável do tipo bool e atribua um valor a ela. Em seguida, imprima essa variável na tela.
+
+bool verdadeiro = true;
+
+Console.WriteLine("Essa variável é do tipo Bool, pode ser True ou False. R= " + verdadeiro);
