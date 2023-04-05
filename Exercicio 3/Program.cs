d6fd184742043629e35f4b606f80ed3cb61d0753
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:23:58 -0300

    Somando duas variáveis

--- Exercicio 3/Program.cs
+++ Exercicio 3/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Crie um programa que declare duas variáveis do tipo inteiro e atribua valores diferentes a elas. Em seguida, realize a soma dessas variáveis e imprima o resultado na tela.
 
-int valorInteiro = 20;
+int valorInteiro = 21;
 int valorInteiro2 = 30;
 
 Console.WriteLine(valorInteiro + valorInteiro2);
