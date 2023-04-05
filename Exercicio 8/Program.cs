923f86434eb31985ad4bfb33542bc1117f4efb75
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:42:58 -0300

    Variavél tipo Decimal

---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,6 @@
+﻿//Crie um programa que declare uma variável do tipo double e atribua um valor a ela. Em seguida, imprima essa variável na tela.
+
+decimal valor = 9.50m;
+decimal valor2 = valor * 2;
+
+Console.WriteLine(valor2);
