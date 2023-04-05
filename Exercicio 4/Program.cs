4151b31e5d313211bdc1b83666a631dbe08c3ece
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:24:39 -0300

    Variável tipo String

--- Exercicio 4/Program.cs
+++ Exercicio 4/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Crie um programa que declare uma variável do tipo string e atribua um valor a ela. Em seguida, imprima essa variável na tela.
 
-string nome = "Flamengo";
+string nome = "Real Madrid";
 
 Console.WriteLine("Essa é uma variável do tipo STRING. R = " + nome);
