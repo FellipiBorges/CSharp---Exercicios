2bf0540d2b93486216de68e7bb8fee1a86873b33
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:28:20 -0300

    Variável tipo Double

---
+++ Exercicio 7/Program.cs
@@ -0,0 +1,5 @@
+﻿//Crie um programa que declare uma variável do tipo double e atribua um valor a ela. Em seguida, imprima essa variável na tela.
+
+double numeroDecimal = 10.7;
+
+Console.WriteLine(numeroDecimal);
