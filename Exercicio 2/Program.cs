61b6dd26ab014333abd8e54bc4d714fe472e6db2
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 22:15:50 -0300

    Variavél tipo String

--- Exercicio 2/Program.cs
+++ Exercicio 2/Program.cs
@@ -1,4 +1,4 @@
 ﻿//Crie um programa que declare uma variável do tipo float e atribua um valor a ela. Em seguida, imprima esse valor na tela.
 
-float f = 105.7f; 
+float f = 105.7f;
 Console.WriteLine("Essa variável é do tipo float e deve ser utilizada com o 'f' no final do resultado. R= " + f);
---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,5 @@
+﻿//Crie um programa que declare uma variável do tipo string e atribua um valor a ela. Em seguida, imprima essa variável na tela.
+
+string nome = "Flamengo";
+
+Console.WriteLine("Essa é uma variável do tipo STRING. R = " + nome);
